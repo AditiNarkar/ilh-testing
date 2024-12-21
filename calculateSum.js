e1f8e2413e073b499ebfbe0c73ce957b252a3c97
Aditi Narkar <adunarkar2004@gmail.com>
2024-12-21 20:22:37 +0530

    chnge1

--- calculateSum.js
+++ calculateSum.js
@@ -1,8 +1,7 @@
-// Original issue: Calculate the sum of a large dataset
 function calculateSum(data) {
   let sum = 0;
-  for (let i = 0; i < data.length; i++) {
-    sum += data[i]; // Summing each element
+  for (let num of data) {
+    sum += num;  // Using for...of loop for summing elements
   }
   return sum;
 }
